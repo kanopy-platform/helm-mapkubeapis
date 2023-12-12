36bf894daf889c2d40d58b201549a1d45481e8a6
Ricardo Hernandez <430024+richerve@users.noreply.github.com>
2023-12-12 17:23:48 +0000

    Update install script (#3)
    * update plugin install script to point to kanopy-platform
    
    * bump release

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -12,18 +12,18 @@ echo "Downloading and installing helm-mapkubeapis v${version} ..."
 url=""
 if [ "$(uname)" = "Darwin" ]; then
     if [ "$(uname -m)" = "arm64" ]; then
-        url="https://github.com/helm/helm-mapkubeapis/releases/download/v${version}/helm-mapkubeapis_${version}_darwin_arm64.tar.gz"
+        url="https://github.com/kanopy-platform/helm-mapkubeapis/releases/download/v${version}/helm-mapkubeapis_${version}_darwin_arm64.tar.gz"
     else
-        url="https://github.com/helm/helm-mapkubeapis/releases/download/v${version}/helm-mapkubeapis_${version}_darwin_amd64.tar.gz"
+        url="https://github.com/kanopy-platform/helm-mapkubeapis/releases/download/v${version}/helm-mapkubeapis_${version}_darwin_amd64.tar.gz"
     fi
 elif [ "$(uname)" = "Linux" ] ; then
     if [ "$(uname -m)" = "aarch64" ] || [ "$(uname -m)" = "arm64" ]; then
-        url="https://github.com/helm/helm-mapkubeapis/releases/download/v${version}/helm-mapkubeapis_${version}_linux_arm64.tar.gz"
+        url="https://github.com/kanopy-platform/helm-mapkubeapis/releases/download/v${version}/helm-mapkubeapis_${version}_linux_arm64.tar.gz"
     else
-        url="https://github.com/helm/helm-mapkubeapis/releases/download/v${version}/helm-mapkubeapis_${version}_linux_amd64.tar.gz"
+        url="https://github.com/kanopy-platform/helm-mapkubeapis/releases/download/v${version}/helm-mapkubeapis_${version}_linux_amd64.tar.gz"
     fi
 else
-    url="https://github.com/helm/helm-mapkubeapis/releases/download/v${version}/helm-mapkubeapis_${version}_windows_amd64.tar.gz"
+    url="https://github.com/kanopy-platform/helm-mapkubeapis/releases/download/v${version}/helm-mapkubeapis_${version}_windows_amd64.tar.gz"
 fi
 
 echo "$url"
